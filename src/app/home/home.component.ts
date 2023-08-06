7d333f01e17c6e275d6a202a2959181b0aeb14e4
Alice Smith <alimay.smith@outlook.com>
2023-08-06 17:40:46 -0600

    init home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ams-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
