86d6cb4be86a106fa7b3ac5efa088d8cb7514353
Alice Smith <alimay.smith@outlook.com>
2025-02-09 14:48:40 -0700

    adding bootstrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'ams-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css'],
-    standalone: false
+  selector: 'ams-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  standalone: false,
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
