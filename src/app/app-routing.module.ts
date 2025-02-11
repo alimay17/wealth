c4269560ec7a60f447f81ed6ebf33c0a31ea71ba
Alice Smith <alimay.smith@outlook.com>
2025-02-10 18:11:45 -0700

    building basic design scaffold

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
+import { NewLogComponent } from './new-log/new-log.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'landing', component: LandingComponent},
+  {path: 'newLog', component: NewLogComponent },
 
   // default
   {path: '', redirectTo: '/home', pathMatch: 'full'}
---
+++ src/app/new-log/new-log.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ams-new-log',
+  templateUrl: './new-log.component.html',
+  styleUrl: './new-log.component.css'
+})
+export class NewLogComponent {
+
+}
